8abf546f44da3bdd05c68b7c6bde0296d013fbc1
Pedro Lemos <lemozxpedro@gmail.com>
2024-02-06 15:40:26 -0300

    refactor: updated example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:example/image_service.dart';
+import 'package:example/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:segregated_data_persistence/segregated_data_persistence.dart';
 
 Future<void> main() async {
-  runApp(const App());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  ServiceLocator.add(DataPersistenceEngine());
+  ServiceLocator.add(ImageService(ServiceLocator.get<DataPersistenceEngine>()));
+
+  runApp(DataPersistence(child: const App()));
 }
 
 class App extends StatelessWidget {
@@ -25,13 +33,38 @@ class InitialPage extends StatefulWidget {
 }
 
 class _InitialPageState extends State<InitialPage> {
+  final images = ValueNotifier<Iterable<PersistedData>>([]);
+
+  Future<void> _getImages() async {
+    final imageService = ServiceLocator.get<ImageService>();
+    images.value = await imageService.getImages();
+  }
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) => _getImages());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Segregated Data Persistence Example',
-        ),
+      appBar: AppBar(title: const Text('Segregated Data Persistence Example')),
+      body: ValueListenableBuilder(
+        valueListenable: images,
+        builder: (context, value, child) {
+          if (value.isEmpty) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.builder(
+            itemCount: value.length,
+            itemBuilder: (context, index) {
+              final image = value.elementAt(index);
+              return PersistedImage(fileName: image.fileName);
+            },
+          );
+        },
       ),
     );
   }
